64cc46d87201c782a4b0ff5b50e330feae73794a
CésarMBat <cesar.morsoleto@gmail.com>
2023-06-05 21:50:57 -0300

    Mudanças em praticamente tudo

--- imc_calc.js
+++ imc_calc.js
@@ -5,17 +5,17 @@ const IMC = () => {
     let resultado = document.querySelector('#resultado')
     let IMC_VALOR = peso / (altura * altura)
     if (IMC_VALOR <= 18.5 && IMC_VALOR > 0) {
-        resultado.innerHTML = `Abaixo do peso saudável - ${IMC_VALOR}`
+        resultado.innerHTML = `Abaixo do peso saudável - ${IMC_VALOR.toFixed(3)}`
     } else if (IMC_VALOR > 18.5 && IMC_VALOR <= 24.9) {
-        resultado.innerHTML = `Peso saudável - ${IMC_VALOR}`
+        resultado.innerHTML = `Peso saudável - ${IMC_VALOR.toFixed(3)}`
     } else if (IMC_VALOR > 24.9 && IMC_VALOR <= 29.9) {
-        resultado.innerHTML = `Levemente acima do peso - ${IMC_VALOR}`
+        resultado.innerHTML = `Levemente acima do peso - ${IMC_VALOR.toFixed(3)}`
     } else if (IMC_VALOR > 29.9 && IMC_VALOR <= 34.9) {
-        resultado.innerHTML = `Obesidade Grau 1 - ${IMC_VALOR}`
+        resultado.innerHTML = `Obesidade Grau 1 - ${IMC_VALOR.toFixed(3)}`
     } else if (IMC_VALOR > 34.9 && IMC_VALOR <= 39.9) {
-        resultado.innerHTML = `Obesidade Grau 2 - ${IMC_VALOR}`
+        resultado.innerHTML = `Obesidade Grau 2 - ${IMC_VALOR.toFixed(3)}`
     } else if (IMC_VALOR > 40) {
-        resultado.innerHTML = `Obesidade Grau 3 (Obesidade mórbida) - ${IMC_VALOR}`
+        resultado.innerHTML = `Obesidade Grau 3 (Obesidade mórbida) - ${IMC_VALOR.toFixed(3)}`
     } else {
         resultado.innerHTML = `Insira valores corretos`
     }
